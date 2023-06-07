62a78639b1ddfbc1de25ac2745e7766abe942fe0
Richmond Gyekye <mrgyekye740@gmail.com>
2023-06-07 10:52:07 +0000

    Initial Commit

---
+++ scrumfy/scrumfyApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumfyApp.swift
+//  scrumfy
+//
+//  Created by richmond gyekye on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct scrumfyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
